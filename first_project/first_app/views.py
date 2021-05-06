542f292e7b0b752ca95d64b5de466c887d1cb436
hatanakashumpei <inovation@keio.jp>
2021-05-06 19:09:40 +0900

    URL ディスパッチ

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 def index(requeset):
     return HttpResponse('<h1>Hello</h1>')
 
+def user_page(request, user_name):
+    return HttpResponse(f'<h1>{user_name}\'s page</h1>')
+
+def number_page(request, user_name, number):
+    user_name = user_name.upper()
+    return HttpResponse(f'<h1>{user_name}\'s page number={number}<h1>')
